eb99a07c74f4a728ae7ddbddc39f0373a04c5354
ibrahim khaleelullah <ibrahim.uta@gmail.com>
2019-10-11 15:10:35 -0400

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsernameComponent } from './username/username.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'username', component: UsernameComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { UsernameComponent } from './username/username.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartComponent,
+    UsernameComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
