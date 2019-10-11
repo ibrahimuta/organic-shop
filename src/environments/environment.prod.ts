ca9827b8497f755803415f702fd83fe625dbca52
ibrahim khaleelullah <ibrahim.uta@gmail.com>
2019-10-11 17:00:43 -0400

    added firebase setting in prod package.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAxmUKcB87vIPa4KN8FTIJAph-vpWZcHcQ',
+    authDomain: 'mosh-angular-pro.firebaseapp.com',
+    databaseURL: 'https://mosh-angular-pro.firebaseio.com',
+    projectId: 'mosh-angular-pro',
+    storageBucket: 'mosh-angular-pro.appspot.com',
+    messagingSenderId: '864346632331',
+    appId: '1:864346632331:web:412d808568a8a5e281648e',
+    measurementId: 'G-YRVB2Q0D1G'
+  }
 };
