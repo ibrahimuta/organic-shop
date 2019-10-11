6b7b623f88e87d6114d4a5d08e1adfc2410f90b9
ibrahim khaleelullah <ibrahim.uta@gmail.com>
2019-10-11 15:46:24 -0400

    Added nav bar component and added bootstrap to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { UsernameComponent } from './username/username.component';
+import { environment } from '../environments/environment';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingCartComponent,
-    UsernameComponent
+    UsernameComponent,
+    BsNavbarComponent
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule, // storage
     AppRoutingModule,
     BrowserAnimationsModule,
     NgbModule.forRoot()
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAxmUKcB87vIPa4KN8FTIJAph-vpWZcHcQ',
+    authDomain: 'mosh-angular-pro.firebaseapp.com',
+    databaseURL: 'https://mosh-angular-pro.firebaseio.com',
+    projectId: 'mosh-angular-pro',
+    storageBucket: 'mosh-angular-pro.appspot.com',
+    messagingSenderId: '864346632331',
+    appId: '1:864346632331:web:412d808568a8a5e281648e',
+    measurementId: 'G-YRVB2Q0D1G'
+  }
 };
 
 /*
